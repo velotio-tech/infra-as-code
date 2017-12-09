40200d88b000f00bea87f7e9e1d14b92ca820020
Chirag Jog <chirag@velotio.com>
2017-12-09 14:51:04 +0530

    update as per jenkin build path

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'chefadmin'
-client_key               '/home/centos/.chef/chefadmin.pem'
+client_key               '/var/lib/jenkins/workspace/build/.chef/chefadmin.pem'
 #validation_client_name   'shortname-validator'
 #validation_key           '/home/centos/.chef/velotio-validator.pem'
 chef_server_url          'https://ec2-34-226-123-132.compute-1.amazonaws.com/organizations/4thcoffee'
-syntax_check_cache_path  '/home/centos/.chef/syntax_check_cache'
-cookbook_path [ '/home/centos/infra-as-code/cookbooks' ]
+syntax_check_cache_path  '/var/lib/jenkins/workspace/build/.chef/syntax_check_cache'
+cookbook_path [ '/var/lib/jenkins/workspace/build/cookbooks' ]
