96ef482482da0dfeb9bc327095d15063b01e4c9e
Azar Maniyar <azar.maniyar@velotio.com>
2017-12-09 15:15:27 +0530

    moved key file

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'chefadmin'
-client_key               '/var/lib/jenkins/workspace/build/.chef/chefadmin.pem'
+client_key               '/opt/keys/chefadmin.pem'
 #validation_client_name   'shortname-validator'
-#validation_key           '/home/centos/.chef/velotio-validator.pem'
+#validation_key           '/opt/keys/velotio-validator.pem'
 chef_server_url          'https://ec2-34-226-123-132.compute-1.amazonaws.com/organizations/4thcoffee'
 syntax_check_cache_path  '/var/lib/jenkins/workspace/build/.chef/syntax_check_cache'
 cookbook_path [ '/var/lib/jenkins/workspace/build/cookbooks' ]
