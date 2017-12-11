ee97888e49299417a1906cf78ce5ee456fe8912a
Cloud User <centos@ip-172-31-58-74.ec2.internal>
2017-12-11 14:08:29 +0000

    Using chef coverage

--- cookbooks/ror-app/spec/spec_helper.rb
+++ cookbooks/ror-app/spec/spec_helper.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 require 'chefspec'
 require 'chefspec/berkshelf'
-require 'simplecov'
-require 'simplecov-json'
-require 'simplecov-rcov'
-
-SimpleCov.formatters = [
-  SimpleCov::Formatter::RcovFormatter
-]
-SimpleCov.start
-
 require_relative './unit/recipes/default_spec.rb'
+at_exit { ChefSpec::Coverage.report! }
 
